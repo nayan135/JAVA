61aa113df182e679d53df811926962fa912aa665
NAYAN ACHARYA <nayan374@proton.me>
2024-07-25 07:39:15 +0545

    Changes

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class App{
+    public static void main(String na[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("NYA");
+        System.out.println("what's your name?");
+        String name = sc.nextLine();
+        System.out.println("enter your age");
+        int age = sc.nextInt();
+        System.out.println("hello "+name);
+        System.out.println("your age is "+age);
+        
+    }
+}
